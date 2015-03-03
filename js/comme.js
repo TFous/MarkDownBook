3199048dbd0df91c023cc155b3386771d028bbeb
TFous <475984203@qq.com>
2015-03-03 11:51:59 +0800

    star
    ok

---
+++ js/comme.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2015/3/3.
+ */
+//目录显示出现
+$(function(){
+    $("body").prepend("<div class=\"showbtn\">Menu</div><div class=\"menu\"> <div class=\"hidebtn\">Menu(点击关闭)</div> <ul id=\"tree\" class=\"ztree\"></ul></div>");
+    $(".showbtn").click(function(){
+        $(".menu").show();
+        $(this).hide();
+    });
+    $(".hidebtn").click(function(){
+        $(".menu").hide();
+        $(".showbtn").show();
+    });
+    $('#tree').ztree_toc({
+        is_auto_number: true
+        // documment_selector: '.first_part'
+    });
+});
+
+
+
+
